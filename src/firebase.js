9fca9406aaf2b15ae243411e7d8e63a6ec71e780
Linda Qin <qinx@college.harvard.edu>
2020-06-08 16:21:33 -0400

    pulling

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,4 @@
 import * as firebase from 'firebase';
-<<<<<<< HEAD
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBwhjyNKWB2stpJFgG9pYHXUuV26s6U1KU",
-  authDomain: "stint-landing.firebaseapp.com",
-  databaseURL: "https://stint-landing.firebaseio.com",
-  projectId: "stint-landing",
-  storageBucket: "stint-landing.appspot.com",
-  messagingSenderId: "989807747408",
-  appId: "1:989807747408:web:fe02b8a376979ad8608c55",
-  measurementId: "G-XLX384B7P4"
-=======
 const config = require("./config.json")
 
 const firebaseConfig = {
@@ -22,7 +10,6 @@ const firebaseConfig = {
   messagingSenderId: config.messagingSenderId,
   appId: config.appId,
   measurementId: config.measurementId
->>>>>>> 2b90759ff080702227371fddf40ca06269477d29
 };
 
 firebase.initializeApp(firebaseConfig);
