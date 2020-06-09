dba65a09a6542a2f8130c752b76796933de8bb2d
Linda Qin <qinx@college.harvard.edu>
2020-06-09 13:22:48 -0400

    switching branches

--- src/components/Company.js
+++ src/components/Company.js
@@ -22,48 +22,6 @@ class Company extends React.Component {
     return (
       <div className="container">
 
-        <div className="modal" style={{display: this.state.modal ? 'block' : 'none'}}>
-            <form onSubmit={this.onCreateMessage}>
-              <div className="flex-row" style={{marginBottom: "40px"}}>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Name"
-                  className="input"
-                  onChange={this.onChangeName}/>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Student email"
-                  className="input"
-                  onChange={this.onChangeEmail}
-                  style={{width: "250px", marginLeft: "20px"}}/>
-              </div>
-
-              <select name="Interest"
-                      className="dropdown"
-                      onChange={this.onChangeDropdown}
-                      style={{width: "300px", marginBottom: "40px"}}>
-                  <option value="" disabled selected>Area of interest</option>
-                  <option value="design" className="dropdown-item">Design</option>
-                  <option value="engineering" className="dropdown-item">Engineering</option>
-                  <option value="creatives" className="dropdown-item">Creatives</option>
-                  <option value="analytics" className="dropdown-item">Analytics</option>
-                </select>
-              <button type="submit"
-                className="button">Join our beta →</button>
-            </form>
-        </div>
-        <div className="modal-screen"
-             style={{display: this.state.modal ? 'block' : 'none'}}
-             onClick={this.handleButtonClick}></div>
-
-
-      <div className="covid">
-      In light of COVID-19, we hope to support students and companies in any way we can. If you are a company looking to hire, we’d love
-      to&nbsp;<a href="mailto:wearestint@gmail.com" className="url">hear from you.</a>
-      </div>
-
       <div className="menu flex-row" style={{justifyContent: 'flex-start'}}>
         <div className="flex-row center">
           <img src={logo} style={{width: '36px'}}/>
@@ -72,57 +30,17 @@ class Company extends React.Component {
       </div>
 
       <div className="flex-row center padding homepage-1" style={{paddingBottom: "100px"}}>
-        <div className="homepage-text">
-          <h1>No internship? <br/>No problem.</h1>
-          <h3>We’re redefining the way students and companies connect through shorter, project-based stints.</h3>
-          <div onClick={this.handleButtonClick}><Button text="Join the revolution" margin="50px"/></div>
-        </div>
         <img src={companyImage} className="homepage-image"/>
-      </div>
-
-      <div className="flex-row interstitial">
-        <div className="interstitial-item">
-          <h2>Build skills.</h2>
-          <p>Bootcamps and extracurriculars are not the only way to obtain real-world experience while still being a full-time student. Gain industry experience, straight from the source.</p>
-        </div>
-
-        <div className="interstitial-item">
-          <h2>Stay productive.</h2>
-          <p>Have some extra time on your hands? Receive stints and earn some money for those next few burritos on your own time and pace.  </p>
-        </div>
-
-        <div className="interstitial-item">
-          <h2>Break boundaries.</h2>
-          <p>We believe in challenging the status quo. Spend your time doing meaningful projects, instead of networking. Highlight your skills, not your connections.</p>
+        <div className="company-text">
+          <h1>Access student talent anywhere, anytime.</h1>
+          <h3>Experience an easier way to find immediate student hires for your company tasks.</h3>
+          <div onClick={this.handleButtonClick}><Button text="Join our beta" margin="50px"/></div>
         </div>
       </div>
 
-      <div className="audience flex-row">
-        <div className="flex-column audience-text" style={{marginTop: "20px"}}>
-          <h1>For</h1>
-          <h1 onClick={() => this.changeName('./imgs/designers.svg', '0')}
-              className="talent"
-              style={{color: this.state.selectedNum == 0 ? "#474448" : "#E7E7E7"}}>designers.</h1>
-          <h1 onClick={() => this.changeName('./imgs/coders.svg', '1')}
-              className="talent"
-              style={{color: this.state.selectedNum == 1 ? "#474448" : "#E7E7E7"}}>engineers.</h1>
-          <h1 onClick={() => this.changeName('./imgs/creatives.svg', '2')}
-              className="talent"
-              style={{color: this.state.selectedNum == 2 ? "#474448" : "#E7E7E7"}}>creatives.</h1>
-            <h1 onClick={() => this.changeName('./imgs/analysts.svg', '3')}
-                  className="talent"
-                  style={{color: this.state.selectedNum == 3 ? "#474448" : "#E7E7E7"}}>analysts.</h1>
-        </div>
-        <div className="browser">
-
-          <div className="flex-column">
-
-          </div>
+      <div className="flex-column center padding company-2">
 
 
-          <img src={require(`${this.state.selectedName}`)}
-            className="typing-image"/>
-        </div>
       </div>
 
       <div className="cta flex-column center" style={{backgroundColor: "#f5f5f5", padding: "150px 10%"}}>
