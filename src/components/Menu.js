2fe4675cc17d8dfa292ff295a3d96d4c189b2032
Linda Qin <qinx@college.harvard.edu>
2020-06-11 16:22:02 -0400

    added menu tabs

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import logo from './imgs/logo.png'
+import { NavLink } from 'react-router-dom';
 
 class Menu extends React.Component {
 
 	render() {
 		return (
-      <div className="menu flex-row" style={{justifyContent: 'flex-start'}}>
-        <div className="flex-row center">
+      <div className="menu flex-row" style={{justifyContent: 'space-between'}}>
+        <a href="/" className="flex-row center logo-container">
           <img src={logo} className="logo-img"/>
           <div className="logo">Stint</div>
-        </div>
+        </a>
+
+        <NavLink to="/hire"
+                 className="menu-item"
+                 activeClassName="active-item">For Companies</NavLink>
       </div>
 		)
 	}
