d1dbc51e539078e40118350874591f988f1ab314
Charles Ma <charlesma@Charless-MBP.hsd1.ma.comcast.net>
2020-06-18 15:41:38 -0400

    Added component and searchOptions constant

--- src/components/StudentInfo.js
+++ src/components/StudentInfo.js
@@ -2,10 +2,17 @@ import React from 'react';
 import {Colleges, Majors} from './LoginDropdowns.js'
 import Select from './Select.js'
 import Autocomplete from './Autocomplete.js'
+import firebaseConfig from '../config.js'
+import PlacesAutocomplete from 'react-places-autocomplete'
 
 const Year = [ "(select year*)", "freshman", "sophomore", "junior", "senior"]
 
 const State = ["(select state*)", "Outside US", "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District Of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+
+const searchOptions = {
+  types: ["(cities"],
+  componentRestrictions: {country: "us"}
+}
 
 class StudentInfo extends React.Component {
 
@@ -21,6 +28,7 @@ class StudentInfo extends React.Component {
 
       <div className="student-dialogue">
         <div className="student-dialogue-block">
+        <script src="http://maps.googleapis.com/maps/api/js?key={firebaseConfig.apiKey}&libraries=places" type="text/javascript"></script>
           <h3>I am a <Select items={Year} name="year"/> at
           <Autocomplete options={Colleges} name="colleges" placeholder="(insert college*)"/>.</h3>
           <h3>I'm majoring in <Autocomplete options={Majors} name="majors" placeholder="(insert major*)"/> and minoring in
