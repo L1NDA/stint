eed34d9dcc6bf87ed7d4a670978aa43c4e2371f6
Linda Qin <qinx@college.harvard.edu>
2020-06-18 14:59:54 -0400

    pushing info section

--- src/components/ProfileCreation.js
+++ src/components/ProfileCreation.js
@@ -3,15 +3,9 @@ import './style/profile.css';
 import Menu from './Menu.js';
 import Footer from './Footer.js'
 import Button from './Button.js'
-import {Colleges, Majors} from './LoginDropdowns.js'
-import Select from './Select.js'
-import Autocomplete from './Autocomplete.js'
+import StudentInfo from './StudentInfo.js'
 import app from 'firebase/app';
 import 'firebase/database';
-
-const Year = [ "(select year*)", "freshman", "sophomore", "junior", "senior"]
-
-const State = ["(select state*)", "Outside US", "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District Of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 
 class ProfileCreation extends React.Component {
 
@@ -20,10 +14,6 @@ class ProfileCreation extends React.Component {
     this.state = {
       continue: false
     }
-  }
-
-  handleChange = () => {
-
   }
 
   render() {
@@ -39,31 +29,8 @@ class ProfileCreation extends React.Component {
           <h3>We’re Stint, a platform for connecting students and companies. <br/> Now tell us a little bit about yourself!</h3>
         </div>
 
-        <div className="student-dialogue">
-          <div className="student-dialogue-block">
-            <h3>I am a <Select items={Year} name="year"/> at
-            <Autocomplete options={Colleges} name="colleges" placeholder="(insert college*)"/>.</h3>
-            <h3>I'm majoring in <Autocomplete options={Majors} name="majors" placeholder="(insert major*)"/> and minoring in
-            <Autocomplete options={Majors} name="minors" placeholder="(insert minor)"/>.</h3>
-            <h3>I’m currently residing in
-              <Autocomplete options={["Boston", "New York City", "San Francisco"]} name="city" placeholder="(insert city*)"/>,
-              <Select items={State} name="state"/>.</h3>
-          </div>
-          <div className="hline"></div>
-          <div className="student-dialogue-block">
-            <h3>My most recent work experience was as a(n)
-              <Autocomplete options={[]} name="role-1" placeholder="(role*)"/>
-              at <Autocomplete options={[]} name="company-1" placeholder="(company*)"/>.</h3>
-            <h3>Another recent work experience was as a(n) <Autocomplete options={[]} name="role-2" placeholder="(role)"/> at <Autocomplete options={[]} name="company-2" placeholder="(company)"/>.</h3>
-            <h3>Another recent work experience was as a(n) <Autocomplete options={[]} name="role-3" placeholder="(role)"/> at <Autocomplete options={[]} name="company-3" placeholder="(company)"/>.</h3>
-          </div>
-          <div className="hline"></div>
-          <div className="student-dialogue-block">
-            <h3>I’ve been most recently involved in <Autocomplete options={[]} name="ec-1" placeholder="(organization*)"/> as a(n) <Autocomplete options={[]} name="ec-role-1" placeholder="(role*)"/>.</h3>
-            <h3>I’ve also been involved in <Autocomplete options={[]} name="ec-2" placeholder="(organization)"/> as a(n) <Autocomplete options={[]} name="ec-role-2" placeholder="(role)"/>.</h3>
-            <h3>I’ve also been involved in <Autocomplete options={[]} name="ec-2" placeholder="(organization)"/> as a(n) <Autocomplete options={[]} name="ec-role-2" placeholder="(role)"/>.</h3>
-          </div>
-        </div>
+        <StudentInfo/>
+
 
         <div className={this.state.continue ? "stint-dialogue" : "loading"}>
           <h2>Look at you, out there doing things!</h2>
