680c51b82265cc6630678dbf53e40a919ebf3bd0
Charles Ma <charlesma@Charless-MBP.local>
2020-06-30 16:17:26 -0400

    Attempting to set up redux

--- src/context/FreelancerContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import firebase from 'firebase'
-
-const FreelancerContext = React.createContext({freelancer: firebase.auth().onAuthStateChanged(freelancer => {return freelancer})});
-
-export const FreelancerProvider = FreelancerContext.Provider
-export const FreelancerConsumer = FreelancerContext.Consumer
-
-export default FreelancerContext
--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import FreelancerProvider from './context/FreelancerContext'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
   <React.StrictMode>
+  <Provider store={store}>
     <App />
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
