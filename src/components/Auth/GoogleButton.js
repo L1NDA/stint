51fc612d426cf4e7237e5a64da57299c754a2364
Linda Qin <qinx@college.harvard.edu>
2020-07-06 14:18:55 -0400

    fixed google button

--- src/components/Auth/GoogleButton.js
+++ src/components/Auth/GoogleButton.js
@@ -8,42 +8,35 @@ const googleButtonStyle = css`
     /* height: 50px; */
     border-width: 0;
     background: white !important;
-    color: #737373 !important;
-    border-radius: 5px;
-    white-space: nowrap;
-    box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.05);
-    transition-property: background-color, box-shadow;
-    transition-duration: 150ms;
-    transition-timing-function: ease-in-out;
-    padding: 0;
-    &:focus,
+    color: #474448 !important;
+    border-radius: 0px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 15px 20px;
+    margin-bottom: 0px;
+    &:focus {
+      outline: none;
+    }
     &:hover {
-      box-shadow: 1px 4px 5px 1px rgba(0, 0, 0, 0.1) !important;
+      cursor: pointer;
     }
     &:active {
-      background-color: #e5e5e5 !important;
-      box-shadow: none;
-      transition-duration: 10ms;
+      /* background-color: #e5e5e5 !important; */
     }
   }
   .google-button__icon {
-    display: inline-block;
-    vertical-align: middle;
-    margin: 0px 0 8px 8px;
-    width: 18px;
-    height: 18px;
-    box-sizing: border-box;
+    margin-right: 10px;
+    width: 15px;
+    height: 15px;
   }
   .google-button__icon--plus {
     width: 27px;
   }
   .google-button__text {
-    display: inline-block;
-    vertical-align: middle;
-    padding: 0 24px;
     font-size: 14px;
     font-weight: bold;
-    font-family: "Roboto", arial, sans-serif;
+    font-family: 'Roboto', sans-serif;
   }
 `;
 
