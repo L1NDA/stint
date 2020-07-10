205f61100943305afbfe923a70c24cd4ce92cef4
Charles Ma <charlesma@Charless-MBP.local>
2020-07-10 15:08:47 -0400

    Get rid of company reads to increase security

--- src/api/company.js
+++ src/api/company.js
@@ -7,17 +7,11 @@ const firebase = require("firebase")
 
 const setCompanyBetaInfo = (name, email, interestAreas) => {
 	const companyRef = firebase.database().ref(COMPANIES_REF_PATH + "/" + name)
-	companyRef.once("value", function(snapshot) {
-		if (snapshot.val() === null) {
-			companyRef.set({
-				COMPANY_EMAIL: email,
-				COMPANY_INTEREST_AREAS: interestAreas
-			})
-		}
-		else {
-			console.log("You have already signed up for the beta!")
-		}
+	companyRef.set({
+		COMPANY_EMAIL: email,
+		COMPANY_INTEREST_AREAS: interestAreas
 	})
+		
 }
 
 module.exports = {
