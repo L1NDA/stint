818809b259bbb3c331dd36a523a6dc49321677c3
Linda Qin <qinx@college.harvard.edu>
2020-07-10 12:30:00 -0400

    404

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Homepage from './components/Homepage.js'
-import Company from './components/Company.js'
+
 import About from './components/About.js'
-import ThankYou from './components/ThankYou.js'
-import PrivatePolicy from './components/PrivatePolicy.js'
 import Auth from './components/Auth'
+import Company from './components/Company.js'
+import FourOhFour from './components/FourOhFour.js'
+import Homepage from './components/Homepage.js'
+import PrivatePolicy from './components/PrivatePolicy.js'
 import ProfileCreation from './components/ProfileCreation'
 import ProfileView from './components/ProfileView'
+import ThankYou from './components/ThankYou.js'
+
 
 function App({ isLoggedIn }) {
   return (
@@ -19,6 +22,7 @@ function App({ isLoggedIn }) {
         <Route path="/hire" component={Company} />
         <Route path="/our-mission" component={About} />
         <Route path="/privacy-policy" component={PrivatePolicy} />
+        <Route component={FourOhFour}/>
       </Switch>
     </Router>
   );
---
+++ src/components/FourOhFour.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import Menu from './Menu.js'
+import Footer from './Footer.js'
+import GoogleButton from './Auth/GoogleButton'
+import { TiTimes } from "react-icons/ti";
+import { Link } from 'react-router-dom';
+
+import { firebaseConnect, isLoaded, isEmpty } from "react-redux-firebase";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { withRouter } from "react-router-dom";
+
+class FourOhFour extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      modal: false,
+    }
+  }
+
+  componentDidMount() {
+    document.title = 'Uh Oh | Stint';
+    console.log(this.props)
+  }
+
+  handleButtonClick = (event) => {
+    event.preventDefault()
+    let temp = this.state.modal;
+    this.setState({
+      modal: !temp
+    });
+  }
+
+  onError = ({ message }) => this.setState({ message, loading: false });
+
+  loginWithProvider = async (provider) => {
+    this.setState({ loading: true });
+    if (!this.props.isLoggedIn) {
+      this.props.loginUser({ provider, onError: this.onError }, () => this.props.analytics.logEvent("login"))
+    }
+    else {
+      this.props.history.push("/this-is-me")
+    }
+  };
+
+	render() {
+    const {loading} = this.state
+		return (
+      <div className="container" style={{height: '100vh'}}>
+        <Menu/>
+        <div className="fof-container">
+          <div className="fof">
+            <img src={require('./imgs/404.svg')} className="fof-img"/>
+            <h2>Well, that’s not good.</h2>
+            <h3>While we fix ourselves up, <br/>why don’t you try the buttons below?‍</h3>
+              {this.props.isLoggedIn ?
+                <Link to="/our-mission" style={{marginTop: "20px"}}><button className="button">About Us</button></Link> :
+                <div className="flex-row center" style={{marginTop: "20px"}}>
+                  <button className="button" onClick={this.handleButtonClick} style={{marginRight: "20px"}}>Join Stint</button>
+                  <Link to="/"><button className="button">Homepage</button></Link>
+                    <div className="modal" style={{display: this.state.modal ? 'flex' : 'none'}}>
+                      <TiTimes className="modal-x" onClick={this.handleButtonClick}/>
+                      <p style={{textAlign: "center", color: "white", textWrap: "balance"}}>Be seen by companies before your coffee is brewed (or your Java compiled 🤓)</p>
+                      <br/>
+                      <GoogleButton
+                        onClick={() => {
+                          !loading && this.loginWithProvider("google");
+                        }}
+                      />
+                      <br/>
+                    </div>
+                    <div className="modal-screen"
+                         style={{display: this.state.modal ? 'block' : 'none'}}
+                         onClick={this.handleButtonClick}></div>
+                </div>}
+
+          </div>
+        </div>
+        <Footer/>
+      </div>
+		)
+	}
+}
+
+function mapStateToProps(state, props) {
+  const { firebase } = props
+  return {
+    loginUser: async ({ provider, onError }) => {
+      try {
+        await firebase.login({ provider: provider, type: "popup" })
+          .then(() => window.location.pathname = "/this-is-me");
+      } catch (err) {
+         onError(err)
+      }
+    },
+    isLoggedIn: state.firebase.auth.uid ? true : false,
+    analytics: firebase.analytics(),
+  };
+}
+
+export default compose(firebaseConnect(), withRouter, connect(mapStateToProps))(FourOhFour);
