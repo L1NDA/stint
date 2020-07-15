903c01af9a85d9d093e29d1692f6d561168a727b
Charles Ma <charlesma@Charless-MBP.local>
2020-07-14 21:24:20 -0400

    Add staging.config

---
+++ src/staging.config.js
@@ -0,0 +1,14 @@
+const INDEX_URL = process.env.NODE_ENV === "development" ? "http://localhost:5001/stint-landing/us-central1/" : "https://www.wearestint.com/"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzzixWsVDXAQnxu1bSq_nKxtmidbSHRl0",
+  authDomain: "stint-staging-eb100.firebaseapp.com",
+  databaseURL: "https://stint-staging-eb100.firebaseio.com",
+  projectId: "stint-staging-eb100",
+  storageBucket: "stint-staging-eb100.appspot.com",
+  messagingSenderId: "169383986878",
+  appId: "1:169383986878:web:bbe35a48de3b4100c580ef",
+  measurementId: "G-3D8HWH7J3R"
+};
+
+module.exports = { firebaseConfig,  INDEX_URL }
