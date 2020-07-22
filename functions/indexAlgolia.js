34aabb7b1e8449f425b6129a946def0f3c31e55e
Leland Wu <lelandwu@Lelands-MacBook-Pro.local>
2020-07-22 18:17:26 -0400

    search round 1

--- functions/indexAlgolia.js
+++ functions/indexAlgolia.js
@@ -77,3 +77,82 @@ database.ref('freelancers').once('value', users => {
         process.exit(1);
       });
   });
+  const { firebaseConfig } = require("./config");
+  const functions = require('firebase-functions');
+  const firebase = require('firebase')
+  const algoliasearch = require('algoliasearch')
+  const admin = require("firebase-admin")
+  
+  const APP_ID = functions.config().algolia.app;
+  const ADMIN_KEY = functions.config().algolia.key;
+  
+  firebase.initializeApp(firebaseConfig);
+  
+  const client = algoliasearch(APP_ID, ADMIN_KEY)
+  
+  const database = firebase.database();
+  
+  
+  /* The index name needs to be the proper one for prod */
+  const index = client.initIndex("staging_users");
+  
+  database.ref('freelancers').once('value', users => {
+      console.log("in script")
+      // Build an array of all records to push to Algolia
+      const records = [];
+      users.forEach(user => {
+        console.log(user)
+        // get the key and data from the snapshot
+        const childKey = user.key;
+        const childData = user.val();
+  
+        if (childData.profile) {
+          if (childData.profile.softwareDev) {
+            let info = childData.profile.softwareDev
+            if (info.skills) {
+              childData.profile.softwareDev.skillsArray = Object.keys(info.skills)
+            }
+          }
+  
+          if (childData.profile.design) {
+            let info = childData.profile.design
+            if (info.skills) {
+              childData.profile.design.skillsArray = Object.keys(info.skills)
+            }
+          }
+  
+          if (childData.profile.dataAnalytics) {
+            let info = childData.profile.dataAnalytics
+            if (info.skills) {
+              childData.profile.dataAnalytics.skillsArray = Object.keys(info.skills)
+            }
+          }
+  
+          if (childData.profile.contentCreation) {
+            let info = childData.profile.contentCreation
+            if (info.skills) {
+              childData.profile.contentCreation.skillsArray = Object.keys(info.skills)
+            }
+          }
+        }
+        console.log(childData.profile)
+        // We set the Algolia objectID as the Firebase .key
+        childData.objectID = childKey;
+        // Add object for indexing
+        records.push(childData);
+  
+      });
+  
+      console.log("ALL RECORDS PUSHED")
+      console.log(records)
+  
+      index
+        .saveObjects(records)
+        .then(() => {
+          console.log('Users imported into Algolia');
+        })
+        .catch(error => {
+          console.error('Error when importing users into Algolia', error);
+          process.exit(1);
+        });
+    });
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,14 +3,17 @@ import algoliasearch from 'algoliasearch/lite';
 import {
   InstantSearch,
   Hits,
-  SearchBox,
   Pagination,
   Highlight,
+  ClearRefinements,
+  RefinementList,
+  Configure,
 } from 'react-instantsearch-dom';
 import PropTypes from 'prop-types';
 import "./style/search.css";
 
 const searchClient = algoliasearch(process.env.REACT_APP_ALGOLIA_ID, process.env.REACT_APP_ALGOLIA_KEY);
+const index = searchClient.initIndex('staging_users');
 
 class Search extends React.Component {
     constructor() {
@@ -18,43 +21,39 @@ class Search extends React.Component {
         this.state = {};
     }
 
+    
+
     render() {
         return (
-          <div>
-            <header className="header">
-              <h1 className="header-title">
-                <a href="/">Searching shit</a>
-              </h1>
-            </header>
-    
-            <div className="container">
-              <InstantSearch searchClient={searchClient} indexName="staging_users">
-                <div className="search-panel">
-                  <div className="search-panel__results">
-                    <SearchBox
-                      className="searchbox"
-                      translations={{
-                        placeholder: '',
-                      }}
-                    />
-                    <Hits hitComponent={Hit} />
-                    <div className="pagination">
-                      <Pagination />
-                    </div>
-                  </div>
-                </div>
-              </InstantSearch>
-            </div>
+          <div className="ais-InstantSearch">
+            <h1>Search</h1>
+            <InstantSearch indexName="staging_users" searchClient={searchClient}>
+              <div className="left-panel">
+                <ClearRefinements/>
+                <h2>Filter</h2>
+                  <RefinementList attribute=  "profile.softwareDev.skillsArray" />
+                  <RefinementList attribute=  "profile.dataAnalytics.skillsArray" />
+                  <RefinementList attribute=  "profile.design.skillsArray" />
+                  <RefinementList attribute=  "profile.contentCreation.skillsArray" />
+                <Configure hitsPerPage={8} />
+              </div>
+              <div className="right-panel">
+                <Hits hitComponent={Hit} />
+                <Pagination />
+              </div>
+            </InstantSearch>
           </div>
         );
       }
 }
 
 function Hit(props) {
+  console.log()
     return (
       <article>
         <h1>
           <Highlight attribute="displayName" hit={props.hit} />
+
         </h1>
       </article>
     );
